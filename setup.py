03bc4cfd7ac289a7098e62cd54c8959902ce682c
Uralstech <106602744+Uralstech@users.noreply.github.com>
2022-07-14 14:12:29 +0530

    ⚙️ Updated setup.py to V0.3.0!

--- setup.py
+++ setup.py
@@ -7,9 +7,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding='utf-8') as fh:
     long_description = '\n' + fh.read()
 
-VERSION = '0.2.0'
+VERSION = '0.3.0'
 DESCRIPTION = 'Helps shorten the amount of code you write'
-LONG_DESCRIPTION = 'A package that shortens the amount of code you have to write for your program with the help of functions.'
 
 # Setting up
 setup(
@@ -17,12 +16,14 @@ setup(
     version=VERSION,
     author="Uralstech (Udayshankar Ravikumar)",
     author_email="<info@uralstech.in>",
+    url="https://github.com/Uralstech/ShrtCde",
+    license="Apache License 2.0",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
     install_requires=[],
-    keywords=['python', 'code', 'short', 'short code', 'ui', 'sort', 'file', 'io', 'library'],
+    keywords=['python', 'shrt', 'short', 'cde', 'code', 'shortcode', 'shrtcde', 'ui', 'sort', 'file', 'io', 'library'],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
